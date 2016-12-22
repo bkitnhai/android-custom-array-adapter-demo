a3da7d0282d3f59db6da5eea459ec48a6c3d97e8
bkitnhai <bkit.n.hai@gmail.com>
2016-12-22 14:03:09 +0100

    this my defined

--- app/src/main/java/com/codepath/example/customadapterdemo/CustomListActivity.java
+++ app/src/main/java/com/codepath/example/customadapterdemo/CustomListActivity.java
@@ -1,18 +1,47 @@
 package com.codepath.example.customadapterdemo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CustomListActivity extends Activity {
 
+	// Creating JSON Parser object
+	JSONParser jParser = new JSONParser();
+
+	ArrayList<HashMap<String, String>> productsList;
+
+	// products JSONArray
+	JSONArray products = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_custom_list);
-		populateUsersList();
+		// Hashmap for ListView
+		productsList = new ArrayList<HashMap<String, String>>();
+
+		// Loading products in Background Thread
+		new LoadAllProducts().execute();
+
+		//populateUsersList();
 	}
 
 	private void populateUsersList() {
@@ -24,5 +53,52 @@ public class CustomListActivity extends Activity {
 		ListView listView = (ListView) findViewById(R.id.lvUsers);
 		listView.setAdapter(adapter);
 	}
-	
+
+	class LoadAllProducts extends AsyncTask<String, String, String> {
+
+		/**
+		 * Before starting background thread Show Progress Dialog
+		 * */
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+
+		}
+
+		/**
+		 * getting All products from url
+		 * */
+		protected String doInBackground(String... args) {
+			// Building Parameters
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			// getting JSON string from URL
+			JSONObject json = jParser.makeHttpRequest("http://wwww.oslophone.com/server/CategoryParse.php", "GET", params);
+			_("test"+json.toString());
+			return null;
+		}
+
+		/**
+		 * After completing background task Dismiss the progress dialog
+		 * **/
+		protected void onPostExecute(String file_url) {
+
+			// updating UI from Background Thread
+			runOnUiThread(new Runnable() {
+				public void run() {
+					/*
+					  Updat`ing parsed JSON data into ListView
+					 */
+					populateUsersList();
+				}
+			});
+
+		}
+
+	}
+
+	public void _(String s){
+		Log.i("app", "Custom" + "##########" + s);
+	}
+
+
 }
